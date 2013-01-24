5e61478596e3f23e1338c83a00adc7f6205b23cc
Albrecht Oster <a.oster@online.de>
2013-01-24 21:26:10 +0100

    changed the way js and css files are included

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
   end
+  
+  def include_js(filename)
+    content_for :js_file, filename.to_s
+  end
+  
+  def include_css(filename)
+    content_for :css_file, filename.to_s
+  end
 end
