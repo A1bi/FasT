2b5e5e731e1930aac898758a8c7924692996f226
Albrecht Oster <a.oster@online.de>
2013-01-24 21:04:36 +0100

    created controller for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 FasT::Application.routes.draw do
+  # dates
   get "termine" => "dates#jedermann", :as => "dates"
 
+  # theater
   get "theater" => "theater#index"
   get "theater/montevideo"
   get "theater/hexenjagd"
   get "theater/medicus"
   get "theater/phantasus"
 
+  # info
   get "info" => "info#index"
   get "info/map"
   get "info/weather"
 
-  get "geschichte" => "history#index"
-
+  # guestbook
   get "g%C3%A4stebuch/neu" => "gbook#new", :as => "gbook_new"
   post "gbook/create"
   get "g%C3%A4stebuch(/:page)" => "gbook#entries", :as => "gbook_entries"
+  
+  # static pages
+  get "geschichte" => "static#history"
+  get "impressum" => "static#impressum"
+  get "satzung" => "static#satzung"
+  get "agb" => "static#agb"
 
-  root :to => 'welcome#index'
+  root :to => 'static#index'
 
 end
