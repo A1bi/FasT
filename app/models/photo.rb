38ec57d30636b14ab66167c338ebc08fcfcc3b36
Albrecht Oster <a.oster@online.de>
2013-02-10 19:53:24 +0100

    bugfixes and adjustments to galleries

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   attr_accessible :gallery_id, :position, :text, :image
-  has_attached_file :image, :styles => { :thumb => "145x145#", :big => "600x600>" }
+  has_attached_file :image, :styles => { :thumb => "145x145#", :big => "600" }
   
   validates :image, :attachment_presence => true
   
