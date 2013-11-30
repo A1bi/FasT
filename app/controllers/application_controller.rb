ef4c5769ce1d587d30a4e076a008ba465556fa94
Albrecht Oster <a.oster@online.de>
2013-11-30 04:45:38 +0100

    added remember me option
    redirects to the page the user tried to open before they were logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::Base
   attr_writer :restricted_to_group
 	
   before_filter :authenticate_user
+  after_filter :reset_goto
   
   protected
   
   def authenticate_user
 		begin
-			@_member ||= Members::Member.find(session[:user_id]) if session[:user_id]
+      session[:user_id] ||= user_id_cookie if user_id_cookie.present?
+			@_member ||= Members::Member.find(session[:user_id]) if session[:user_id].present?
 		rescue
 			session[:user_id] = nil
 		end
@@ -17,7 +19,10 @@ class ApplicationController < ActionController::Base
   end
   
   def restrict_access
+    @skip_reset_goto = true
     if !@_member.id
+      session[:goto_after_login] = request.original_url
+      @skip_reset_goto = false
       return redirect_to members_login_path, :flash => { :warning => t("application.login_required") }
     elsif ![:admin, @restricted_to_group].include? @_member.group_name
       return redirect_to members_root_path, :alert => t("application.access_denied")
@@ -42,4 +47,26 @@ class ApplicationController < ActionController::Base
 	def disable_member_controls
 		@no_member_controls = true
 	end
+  
+  def user_id_cookie
+    cookies.signed[user_id_cookie_name]
+  end
+  
+  def user_id_cookie=(value)
+    cookies.permanent.signed[user_id_cookie_name] = value
+  end
+  
+  def delete_user_id_cookie
+    cookies.delete user_id_cookie_name
+  end
+  
+  private
+  
+  def user_id_cookie_name
+    "_#{Rails.application.class.parent_name}_user_id"
+  end
+  
+  def reset_goto
+    session.delete(:goto_after_login) if !@skip_reset_goto
+  end
 end
--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,14 +1,16 @@
 module Members
 	class SessionsController < BaseController
 		ignore_restrictions
+    skip_filter :reset_goto
 	
 	  def create
 	    member = Member.where({:email => params[:email]}).first
 	    if member && member.authenticate(params[:password])
 	      session[:user_id] = member.id
+        self.user_id_cookie = member.id if params[:remember].present?
 				member.logged_in
 				member.save
-	      redirect_to members_root_path
+	      redirect_to session[:goto_after_login] || members_root_path
 	    else
 	      flash.now.alert = t("members.sessions.auth_error")
 	      render :new
@@ -17,6 +19,7 @@ module Members
 
 	  def destroy
 	    session[:user_id] = nil
+      delete_user_id_cookie
 	    redirect_to root_path, :notice => t("members.sessions.logout")
 	  end
 	end
