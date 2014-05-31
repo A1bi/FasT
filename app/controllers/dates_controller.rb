4680bd3ef5da16838299f815cf8e7caf4af1b02f
Albrecht Oster <a.oster@online.de>
2014-05-31 21:02:44 +0200

    added new index and dates page

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,6 +1,17 @@
 class DatesController < ApplicationController
-  def jedermann
+  before_filter :prepare_ticket_prices
+  
+  def don_camillo
     @event = Ticketing::Event.current
+  end
+  
+  def jedermann
+    @event = Ticketing::Event.first
+  end
+  
+  private
+  
+  def prepare_ticket_prices
     @ticket_types = Ticketing::TicketType.exclusive(false).order("price DESC")
   end
 end
