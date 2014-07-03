8f49de72a2a94274016284ddfae969e406edb778
Albrecht Oster <a.oster@online.de>
2014-07-03 21:23:06 +0200

    added percentage to statistics

--- app/controllers/ticketing/statistics_controller.rb
+++ app/controllers/ticketing/statistics_controller.rb
@@ -81,22 +81,42 @@ module Ticketing
           if ticket.order.is_a? Web::Order
             scopes << stats[:web]
           elsif ticket.order.is_a? Retail::Order
-            scopes << ((stats[:retail][:stores] ||= {})[ticket.order.store_id] ||= {})
+            store_scope = stats[:retail][:stores][ticket.order.store_id] ||= {}
+            scopes << store_scope
             scopes << stats[:retail][:total]
           end
             
           scopes.each do |scope|
-            (scope[ticket.date.id] ||= {})[ticket.type.id] = (scope[ticket.date.id][ticket.type.id] || 0).next
-            scope[ticket.date.id][:total] = (scope[ticket.date.id][:total] || 0).next
-            scope[ticket.date.id][:revenue] = (scope[ticket.date.id][:revenue] || 0) + ticket.price
-            (scope[:total] ||= {})[ticket.type.id] = (scope[:total][ticket.type.id] || 0).next
-            scope[:total][:total] = (scope[:total][:total] || 0).next
-            scope[:total][:revenue] = (scope[:total][:revenue] || 0) + ticket.price
+            [scope[ticket.date.id] ||= {}, scope[:total] ||= {}].each do |inner_scope|
+              increment_stats_values(inner_scope, ticket.type.id, ticket.price)
+            end
           end
+        end
+        
+        scopes = [stats[:web], stats[:retail][:total], stats[:total]]
+        Retail::Store.all.each { |store| scopes << stats[:retail][:stores][store.id] }
+        scopes.each do |scope|
+          next if !scope
+          @dates.each do |date|
+            calc_percentage(scope[date.id], false)
+          end
+          calc_percentage(scope[:total], true)
         end
         
         stats
       end
+    end
+    
+    def increment_stats_values(scope, ticket_type, ticket_price)
+      scope[ticket_type] = (scope[ticket_type] || 0) + 1
+      scope[:total] = (scope[:total] || 0) + 1
+      scope[:revenue] = (scope[:revenue] || 0) + ticket_price
+    end
+    
+    def calc_percentage(scope, all_dates)
+      return if !scope
+      @seats ||= Seat.count.to_f
+      scope[:percentage] = (scope[:total] / (@seats * (all_dates ? @dates.count : 1)) * 100).floor
     end
     
     def prepare_vars
