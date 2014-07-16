b8e931a8071e107117a488075e1da616256c0a90
Albrecht Oster <a.oster@online.de>
2014-07-16 13:31:42 +0200

    index page shows sold out status

--- app/models/ticketing/event.rb
+++ app/models/ticketing/event.rb
@@ -1,11 +1,20 @@
-class Ticketing::Event < BaseModel
-  has_many :dates, :class_name => Ticketing::EventDate
+module Ticketing
+  class Event < BaseModel
+    has_many :dates, class_name: EventDate
   
-  def self.by_identifier(id)
-    where(identifier: id).first
-  end
+    def self.by_identifier(id)
+      where(identifier: id).first
+    end
   
-  def self.current
-    last
+    def self.current
+      last
+    end
+  
+    def sold_out?
+      dates.each do |date|
+        return false if !date.sold_out?
+      end
+      true
+    end
   end
 end
