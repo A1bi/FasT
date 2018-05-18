ba410df9acbb9f55bbdb866f983193dbd42c99b7
Albrecht Oster <a.oster@online.de>
2018-05-18 11:46:25 +0200

    improve caching

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -1,7 +1,3 @@
 class BaseModel < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.collection_cache_key(collection, timestamp_column)
-    [table_name, collection.maximum(timestamp_column).to_i, collection.except(:limit).count].join("/")
-  end
 end
