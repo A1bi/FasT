134c6f78c95302ae920722e160b6737476a33f2b
Albrecht Oster <a.oster@online.de>
2018-06-04 23:30:12 +0200

    refactor static#index add info for sale start

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,6 @@
 class StaticController < ApplicationController
+  def index
+    @event = Ticketing::Event.current
+    @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
+  end
 end
