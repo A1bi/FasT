c3fa48fce2aa1a867cb96fccb6e9dead3cea11bc
Albrecht Oster <a.oster@online.de>
2018-06-08 01:14:34 +0200

    remove final newline from partial rendering

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -4,4 +4,10 @@ module MailerHelper
     attachments.inline[name] = File.read("#{Rails.root}/public/images/mail/#{filename}")
     image_tag attachments[name].url
   end
+
+  def render_inline(template, options = {})
+    # remove final newline from partial
+    # so it will not add whitespace to mail content or even line breaks in text mails
+    render(template, options).chomp.html_safe
+  end
 end
