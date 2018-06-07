90938adf09fa59b3a1f18f71c04d2ceb97548893
Albrecht Oster <a.oster@online.de>
2018-06-08 01:54:33 +0200

    fix regular attachments not showing up in some clients when sending inline attachments

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -8,5 +8,27 @@ class BaseMailer < ActionMailer::Base
   def mail(opts)
     opts[:to] = "albo@a0s.de" if Rails.env.development?
     super
+    fix_mixed_attachments
+  end
+
+  # fix regular attachments not showing up in some clients when sending inline attachments
+  # source: https://github.com/rails/rails/issues/2686#issuecomment-20186734
+  def fix_mixed_attachments
+    # do nothing if we have no actual attachments
+    return if @_message.parts.select { |p| p.attachment? && !p.inline? }.none?
+
+    mail = Mail.new
+
+    related = Mail::Part.new
+    related.content_type = @_message.content_type
+    @_message.parts.select { |p| !p.attachment? || p.inline? }.each { |p| related.add_part(p) }
+    mail.add_part related
+
+    mail.header = @_message.header.to_s
+    mail.content_type = nil
+    @_message.parts.select { |p| p.attachment? && !p.inline? }.each { |p| mail.add_part(p) }
+
+    @_message = mail
+    wrap_delivery_behavior!(delivery_method.to_sym)
   end
 end
