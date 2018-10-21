d5c406245d28cf0c98453352e19205b6033e5547
Albrecht Oster <albrecht@oster.online>
2018-10-21 18:21:08 +0200

    add reference to SahneMixx to front page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def index
     @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
+    @sahnemixx = Ticketing::Event.find_by(identifier: 'sahnemixx')
   end
 end
