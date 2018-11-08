b725c0679fe02b992e2a43a0bdf8806c0b94d7d4
Albrecht Oster <albrecht@oster.online>
2018-11-08 17:02:25 +0100

    add proper dates page for Herdmanns

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,65 +1,58 @@
 class DatesController < ApplicationController
   include ActionView::Helpers::AssetUrlHelper
 
-  def teaser
-    @herdmanns = Ticketing::Event.find_by(identifier: 'herdmanns')
+  def index
+    event = Ticketing::Event.current.last
+    redirect_to dates_event_path(event.slug)
   end
 
-  def event
-    @event = Ticketing::Event.find_by!(identifier: params[:slug])
+  def teaser; end
+
+  def show_event
+    @event = Ticketing::Event.current.find_by!(slug: params[:slug])
     @ticket_types = @event.ticket_types.exclusive(false).order(price: :desc)
+    render "event_#{@event.identifier}"
   end
 
   private
 
-  def structured_data
-    data = []
-
+  def structured_data(event, image: nil, creative_work_url: nil)
     # TODO: add availability state for 'not on sale anymore'
-
-    @event.dates.each do |date|
-      offers = []
-      @ticket_types.each do |type|
-        offers << {
-          url: new_ticketing_order_url(@event.slug),
-          name: type.name,
-          category: "primary",
-          price: type.price,
-          priceCurrency: "EUR",
-          validFrom: @event.sale_start.iso8601,
-          availability: date.sold_out? ? "http://schema.org/SoldOut" : "http://schema.org/InStock"
-        }
-      end
-
-      event = {
-        "@context" => "http://schema.org",
-        "@type" => "TheaterEvent",
-        name: @event.name,
-        image: @event_image ? asset_url(@event_image) : nil,
+    event.dates.map do |date|
+      {
+        '@context' => 'http://schema.org',
+        '@type' => 'TheaterEvent',
+        name: event.name,
+        image: image ? asset_url(image) : nil,
         url: @event_url,
         startDate: date.date.iso8601,
         doorTime: date.door_time.iso8601,
         location: {
-          "@type" => "PerformingArtsTheater",
-          name: "Freilichtbühne am schiefen Turm",
+          '@type' => 'PerformingArtsTheater',
+          name: 'Freilichtbühne am schiefen Turm',
           sameAs: root_url,
-          address: "Burgstraße 2, 56759 Kaisersesch"
+          address: event.location
         },
-        offers: offers
+        offers: event.ticket_types.exclusive(false).map do |type|
+          {
+            url: new_ticketing_order_url(event.slug),
+            name: type.name,
+            category: 'primary',
+            price: type.price,
+            priceCurrency: 'EUR',
+            validFrom: event.sale_start.iso8601,
+            availability: date.sold_out? ? 'http://schema.org/SoldOut' : 'http://schema.org/InStock'
+          }
+        end,
+        workPerformed: if @creative_work_url
+                         {
+                           '@type' => 'CreativeWork',
+                           name: event.name,
+                           sameAs: creative_work_url
+                         }
+                       end
       }
-
-      if @creative_work_url
-        event[:workPerformed] = {
-          "@type" => "CreativeWork",
-          name: @event.name,
-          sameAs: @creative_work_url
-        }
-      end
-
-      data << event
     end
-
-    data
   end
   helper_method :structured_data
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # dates
   scope controller: :dates, path: "termine", as: :dates do
-    root action: :teaser, as: ""
+    root action: :index, as: ''
+    get ':slug', action: :show_event, as: :event
   end
 
   # theater
