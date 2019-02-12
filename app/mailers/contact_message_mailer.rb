b4aae2393197a9d7894fa6a1da021a953915bc52
Albrecht Oster <albrecht@oster.online>
2019-02-12 19:54:10 +0100

    handle spam rejection when delivering contact messages
    add poster to press material

--- app/mailers/contact_message_mailer.rb
+++ app/mailers/contact_message_mailer.rb
@@ -3,6 +3,8 @@ class ContactMessageMailer < BaseMailer
           reply_to: nil,
           return_path: default[:from]
 
+  rescue_from Net::SMTPFatalError, with: :log_spam_rejection
+
   def contact_message(name, email, phone, subject, content)
     @name = name
     @email = email
@@ -10,5 +12,12 @@ class ContactMessageMailer < BaseMailer
     @content = content
 
     mail from: "#{name}<#{email}>", subject: "#{subject} (#{t('contact_messages.via')})"
+  end
+
+  private
+
+  def log_spam_rejection(exception)
+    raise unless exception.message.downcase.include? 'spam'
+    Rails.logger.info "Message with subject '#{message.subject}' rejected as spam by MTA"
   end
 end
