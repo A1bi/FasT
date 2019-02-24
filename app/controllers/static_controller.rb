259335712bc17e175d330158cca591652f2795bf
Albrecht Oster <a.oster@online.de>
2019-02-23 21:39:23 -0800

    add microsite for new play

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,12 @@ class StaticController < ApplicationController
     @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
     @events = Ticketing::Event.where(identifier: %i[sahnemixx gemetzel])
   end
+
+  private
+
+  def theater_play_path_exists?(event)
+    template_exists?("theater/#{event.identifier}")
+  end
+
+  helper_method :theater_play_path_exists?
 end
