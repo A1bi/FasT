e6625a48989c77c035ad6135f59c221a4f4b50fe
Albrecht Oster <a.oster@online.de>
2019-08-11 10:19:03 +0200

    summer season end update
    remove info about Glöckner
    add teaser for fall play
    remove 2019 preview

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def index
     @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
-    @events = Ticketing::Event.where(identifier: %i[sahnemixx gloeckner])
+    @events = Ticketing::Event.where(identifier: %i[sahnemixx])
   end
 
   private
