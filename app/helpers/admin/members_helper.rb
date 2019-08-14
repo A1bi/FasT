65ce720f5d92881d84d47a0bb2767b4ce35bab08
Albrecht Oster <albrecht@oster.online>
2019-08-14 18:46:22 +0200

    fix FAST-8J, show last login in member details

--- app/helpers/admin/members_helper.rb
+++ app/helpers/admin/members_helper.rb
@@ -4,7 +4,16 @@ module Admin
       if member.created_at < Date.new(2013, 4, 1)
         return content_tag(:em, t('admin.members.joined_date_unknown'))
       end
+
       l member.created_at.to_date, format: '%-d. %B %Y'
+    end
+
+    def last_login_time(member)
+      if member.last_login.nil?
+        return content_tag :em, t('admin.members.never_logged_in')
+      end
+
+      l member.created_at.to_date, format: '%-d. %B %Y, %H:%M Uhr'
     end
   end
 end
