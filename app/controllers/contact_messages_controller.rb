92d851307b1244357360369683e5f1896988b087
Albrecht Oster <albrecht@oster.online>
2019-08-18 15:19:05 +0200

    filter out spam messages through a honeypot

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,4 +1,6 @@
 class ContactMessagesController < ApplicationController
+  before_action :filter_spam, only: :create
+
   def index
     @message = ContactMessage.new
   end
@@ -7,7 +9,7 @@ class ContactMessagesController < ApplicationController
     @message = ContactMessage.new(message_params)
 
     if @message.mail
-      redirect_to contact_messages_path, notice: t('contact_messages.success')
+      redirect_to contact_messages_path, notice: t('.success')
     else
       render action: :index
     end
@@ -15,7 +17,16 @@ class ContactMessagesController < ApplicationController
 
   private
 
+  def filter_spam
+    return if params[:comment].blank?
+
+    Raven.capture_message('rejected spam message')
+    redirect_to contact_messages_path
+  end
+
   def message_params
-    params.require(:contact_message).permit(:name, :email, :phone, :subject, :content)
+    params.require(:contact_message).permit(
+      :name, :email, :phone, :subject, :content
+    )
   end
 end
