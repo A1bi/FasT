9e6e76e098ce418baab454e718fc0cfe62a0ca10
Albrecht Oster <albrecht@oster.online>
2019-09-14 00:52:44 +0200

    clean up remaining orders controller under main namespace

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,34 +3,23 @@ class OrdersController < ApplicationController
 
   before_action :find_records
 
-  def show
-    @show_wallet = request.user_agent.match(/(Android|iP(hone|ad|od)|OS X|Windows Phone)/).present?
-  end
+  def show; end
 
   def check_email
-    if @order.is_a?(Ticketing::Web::Order) && @order.email.present? && @order.email == params[:email]
-      redirect_to order_overview_path(@order.signed_info(authenticated: true))
-    else
-      redirect_to request.path, alert: t("orders.overview.wrong_email")
-    end
+    return redirect_to authenticated_overview_path if email_correct?
+
+    redirect_to request.path, alert: t('.wrong_email')
   end
 
   def passbook_pass
     return head 403 if @ticket.blank?
 
-    send_file @ticket.passbook_pass(create: true).file_path, type: 'application/vnd.apple.pkpass'
+    send_file @ticket.passbook_pass(create: true).file_path,
+              type: 'application/vnd.apple.pkpass'
   end
 
   def seats
-    render_cached_json [:ticketing, :orders, :show, @order, @order.date.tickets] do
-      seats = {}
-      [[:chosen, @order], [:taken, @order.date]].each do |type|
-        seats[type.first] = type.last.tickets.where(invalidated: false).map do |t|
-          t.seat&.id
-        end.compact
-      end
-      seats
-    end
+    render json: seats_hash
   end
 
   private
@@ -47,6 +36,31 @@ class OrdersController < ApplicationController
       return redirect_to root_url
     end
 
-    @authenticated = info.authenticated.nonzero? || !@order.is_a?(Ticketing::Web::Order)
+    @authenticated = info.authenticated.nonzero? || !web_order?
   end
+
+  def seats_hash
+    types = [[:chosen, @order], [:taken, @order.date]]
+    types.each_with_object({}) do |type, obj|
+      obj[type.first] = type.last.tickets.where(invalidated: false)
+                            .map(&:seat_id).compact
+    end
+  end
+
+  def web_order?
+    @order.is_a? Ticketing::Web::Order
+  end
+
+  def email_correct?
+    web_order? && @order.email.present? && @order.email == params[:email]
+  end
+
+  def authenticated_overview_path
+    order_overview_path(@order.signed_info(authenticated: true))
+  end
+
+  def show_wallet?
+    @show_wallet ||= request.user_agent.match(WALLET_PATTERN).present?
+  end
+  helper_method :show_wallet?
 end
