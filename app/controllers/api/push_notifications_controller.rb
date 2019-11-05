7521a4f285604bd059887c05d13f7540b0ce3b88
Albrecht Oster <albrecht@oster.online>
2019-11-05 11:29:35 +0100

    fix push notifications controller (fix FAST-AM)

--- app/controllers/api/push_notifications_controller.rb
+++ app/controllers/api/push_notifications_controller.rb
@@ -1,16 +1,14 @@
 module Api
   class PushNotificationsController < ApiController
-    before_action :prepare_device
-
     def register
-      @device.settings = params.require(:settings).permit(:sound_enabled)
-      head @device.save ? :ok : :unprocessable_entity
+      device.settings = params.require(:settings).permit(:sound_enabled)
+      head device.save ? :ok : :unprocessable_entity
     end
 
     private
 
-    def prepare_device
-      @device = Ticketing::PushNotifications::Device.find_or_initialize_by(
+    def device
+      @device ||= ::Ticketing::PushNotifications::Device.find_or_initialize_by(
         token: params[:token], app: params[:app]
       )
     end
