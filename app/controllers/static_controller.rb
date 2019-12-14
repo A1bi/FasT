8a257faf4f39e7a5c4fae23f0bc63670a207e37b
Albrecht Oster <albrecht@oster.online>
2019-12-15 00:30:26 +0100

    updates for new play

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,7 @@ class StaticController < ApplicationController
 
   def index
     @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
-    @events = Ticketing::Event.where(identifier: %i[])
+    @events = Ticketing::Event.where(identifier: %i[gemetzel])
   end
 
   private
