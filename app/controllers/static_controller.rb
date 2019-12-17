91b8989adde843e448b875365e8966760debd8af
Albrecht Oster <albrecht@oster.online>
2019-12-17 23:14:58 +0100

    add separate templates for new event

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,7 @@ class StaticController < ApplicationController
 
   def index
     @alert_file = Rails.root.join('public', 'uploads', 'index_alert.json')
-    @events = Ticketing::Event.where(identifier: %i[gemetzel])
+    @events = Ticketing::Event.where(identifier: %i[gemetzel_cochem])
   end
 
   private
