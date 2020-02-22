0d7523122e8435b06671fa333d9c2045b175c817
Albrecht Oster <albrecht@oster.online>
2020-02-22 12:47:26 +0100

    make statistics concern a little cleaner

--- app/models/concerns/ticketing/statistics.rb
+++ app/models/concerns/ticketing/statistics.rb
@@ -5,45 +5,7 @@ module Ticketing
     def ticket_stats_for_dates(dates)
       Rails.cache.fetch [:ticketing, :statistics, dates, Ticket.all,
                          Reservation.all] do
-        stats = {
-          web: {},
-          retail: {
-            stores: {},
-            total: {}
-          },
-          box_office: {
-            box_offices: {},
-            total: {}
-          },
-          total: {}
-        }
-
-        Ticket.includes(:order, :date, :type, :cancellation)
-              .where(date: dates).find_each do |ticket|
-          next if ticket.cancelled?
-
-          scopes = [stats[:total]]
-          if ticket.order.is_a? Web::Order
-            scopes << stats[:web]
-          elsif ticket.order.is_a? Retail::Order
-            store_scope = stats[:retail][:stores][ticket.order.store_id] ||= {}
-            scopes << store_scope
-            scopes << stats[:retail][:total]
-          elsif ticket.order.is_a? BoxOffice::Order
-            store_scope =
-              stats[:box_office][:box_offices][ticket.order.box_office_id] ||=
-                {}
-            scopes << store_scope
-            scopes << stats[:box_office][:total]
-          end
-
-          scopes.each do |scope|
-            [scope[ticket.date.id] ||= {},
-             scope[:total] ||= {}].each do |inner_scope|
-              increment_stats_values(inner_scope, ticket.type.id, ticket.price)
-            end
-          end
-        end
+        calc_ticket_numbers(dates)
 
         dates.each do |date|
           calc_percentage_of_booked_seats(stats[:total][date.id], [date])
@@ -70,6 +32,50 @@ module Ticketing
     end
 
     private
+
+    def stats
+      @stats ||= {
+        web: {},
+        retail: {
+          stores: {},
+          total: {}
+        },
+        box_office: {
+          box_offices: {},
+          total: {}
+        },
+        total: {}
+      }
+    end
+
+    def calc_ticket_numbers(dates)
+      Ticket.includes(:order, :date, :type, :cancellation)
+            .where(date: dates).find_each do |ticket|
+        next if ticket.cancelled?
+
+        scopes = [stats[:total]]
+        if ticket.order.is_a? Web::Order
+          scopes << stats[:web]
+        elsif ticket.order.is_a? Retail::Order
+          store_scope = stats[:retail][:stores][ticket.order.store_id] ||= {}
+          scopes << store_scope
+          scopes << stats[:retail][:total]
+        elsif ticket.order.is_a? BoxOffice::Order
+          store_scope =
+            stats[:box_office][:box_offices][ticket.order.box_office_id] ||=
+              {}
+          scopes << store_scope
+          scopes << stats[:box_office][:total]
+        end
+
+        scopes.each do |scope|
+          [scope[ticket.date.id] ||= {},
+           scope[:total] ||= {}].each do |inner_scope|
+            increment_stats_values(inner_scope, ticket.type.id, ticket.price)
+          end
+        end
+      end
+    end
 
     def calc_percentage_of_booked_seats(scope, dates)
       return if scope.blank?
