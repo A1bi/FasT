0086aa0338610e6705290d8865478b284440e227
Albrecht Oster <albrecht@oster.online>
2020-03-01 00:44:30 +0100

    fix cron command prefix and add MAILTO

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,12 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
+env 'MAILTO', 'albrecht@oster.online'
+
+# whenever does not need to prefix jobs with bash to load the environment
+# as we are not using RVM in production
+set :job_template, nil
+
 every :day do
   runner 'Ticketing::BadgeResetPushNotificationsJob.perform_later'
 end
