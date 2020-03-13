9022954f481024ddb6f4d4ec0cdaf85d8c14c626
Albrecht Oster <albrecht@oster.online>
2020-03-13 18:58:09 +0100

    remove references to cancelled event

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,7 +5,7 @@ class StaticController < ApplicationController
 
   def index
     @alert_file = Rails.root.join('public/uploads/index_alert.json')
-    @events = Ticketing::Event.where(identifier: %i[gemetzel_2020])
+    @events = Ticketing::Event.where(identifier: %i[])
   end
 
   private
