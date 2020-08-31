49f8d7aba854171196774989ba79ba5262a53919
Albrecht Oster <albrecht@oster.online>
2020-08-31 20:32:14 +0200

    refactor front page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class StaticController < ApplicationController
+  ALERT_FILE_PATH = Rails.root.join('public/uploads/index_alert.json')
+
   skip_authorization
 
   def index
-    @alert_file = Rails.root.join('public/uploads/index_alert.json')
     @events = Ticketing::Event.where(identifier: %i[mit_abstand])
   end
 
@@ -14,5 +15,18 @@ class StaticController < ApplicationController
     template_exists?("theater/#{event.identifier}")
   end
 
-  helper_method :theater_play_path_exists?
+  def show_alert?
+    File.exist? ALERT_FILE_PATH
+  end
+
+  def alert_mtime
+    File.mtime(ALERT_FILE_PATH).to_i
+  end
+
+  def alert_info
+    JSON.parse(File.read(ALERT_FILE_PATH), symbolize_names: true)
+  end
+
+  helper_method :theater_play_path_exists?, :show_alert?, :alert_mtime,
+                :alert_info
 end
