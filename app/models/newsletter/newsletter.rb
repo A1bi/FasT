b828f56eb01c4e1fa7c09139609358f768ef492c
Albrecht Oster <albrecht@oster.online>
2020-12-03 18:35:27 +0100

    allow uploaded images to be embedded in html part of newsletters as well
    add utility classes to style newsletters

--- app/models/newsletter/newsletter.rb
+++ app/models/newsletter/newsletter.rb
@@ -34,31 +34,40 @@ module Newsletter
     end
 
     def body_text_final
-      body_text.gsub(IMAGE_REGEXP, '')
+      remove_image_placeholder
     end
 
     def body_html_final
-      if body_html.present? && body_html.length > 5
-        body_html
-      else
-        html = simple_format(body_text)
-        html.gsub!(
-          %r{<p>((%%bild_\d+%%)+)</p>}, '<p style="text-align: center;">\1</p>'
-        )
-        html.gsub!(IMAGE_REGEXP) do |match|
-          image = images.find_by(id: match.match(/\d+/).to_s)
-          if image.present?
-            link_to(image_tag(image.image.url(:mail), alt: ''),
-                    image.image.url(:big))
-          else
-            ''
-          end
-        end
-        html
-      end
+      html = if body_html.present? && body_html.length > 5
+               body_html
+             else
+               body_html_from_text
+             end
+      fill_image_placeholder(html)
     end
 
     private
+
+    def remove_image_placeholder
+      body_text.gsub(IMAGE_REGEXP, '')
+    end
+
+    def body_html_from_text
+      simple_format(body_text)
+        .gsub(
+          %r{<p>((%%bild_\d+%%)+)</p>}, '<p style="text-align: center;">\1</p>'
+        )
+    end
+
+    def fill_image_placeholder(html)
+      html.gsub(IMAGE_REGEXP) do |match|
+        image = images.find_by(id: match.match(/\d+/).to_s)
+        next '' if image.nil?
+
+        link_to(image_tag(image.image.url(:mail), alt: ''),
+                image.image.url(:big))
+      end
+    end
 
     def send_review_notification
       ApplicationMailer.mail(to: Settings.newsletters.review_email,
