fcb226d68aecb1001981f7f3ab695703f02e0d0b
Albrecht Oster <albrecht@oster.online>
2020-12-06 19:33:51 +0100

    redo coupon PDF specs
    remove before/after hooks
    achieve the same speed improvement by stubbing SVG files

--- spec/lib/ticketing/coupon_pdf_spec.rb
+++ spec/lib/ticketing/coupon_pdf_spec.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
+require_shared_examples 'pdf'
+
 RSpec.describe Ticketing::CouponPdf do
-  # speed up specs by using the same generated PDF for all examples
-  before(:context) do
-    @coupon = create(:coupon, amount: 12.34)
-    @pdf = described_class.new(@coupon).render
-  end
+  let(:coupon) { create(:coupon, amount: 12.34) }
+  let(:pdf) { described_class.new(coupon).render }
+  let(:text_analysis) { PDF::Inspector::Text.analyze(pdf) }
+  let(:page_analysis) { PDF::Inspector::Page.analyze(pdf) }
+  let(:page_layout) { [841.89, 595.28] }
 
-  after(:context) { @coupon.destroy }
+  include_examples 'stub loading of SVG files'
 
-  let(:text_analysis) { PDF::Inspector::Text.analyze(@pdf) }
-  let(:page_analysis) { PDF::Inspector::Page.analyze(@pdf) }
-
-  it 'has one page' do
-    expect(page_analysis.pages.size).to eq(1)
-  end
-
-  it 'has the correct layout' do
-    expect(page_analysis.pages[0][:size]).to eq([841.89, 595.28])
-  end
+  include_examples 'it has the correct number of pages', 1
+  include_examples 'all pages have the correct layout'
 
   it 'contains the correct coupon information' do
     expect(text_analysis.strings)
-      .to include('Gutschein', @coupon.code, '12,34 ', '€')
+      .to include('Gutschein', coupon.code, '12,34 ', '€')
   end
 end
---
+++ spec/support/shared_examples/pdf.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec::Matchers.define :an_svg_file do
+  match { |path| path.to_s.match?(/.+\.svg\z/) }
+end
+
+RSpec.shared_examples 'stub loading of SVG files' do
+  before do
+    allow(File).to receive(:read).with(an_svg_file).and_return('<svg></svg>')
+  end
+end
+
+RSpec.shared_examples 'it has the correct number of pages' do |pages|
+  it "has #{pages} page" do
+    expect(page_analysis.pages.size).to eq(pages)
+  end
+end
+
+RSpec.shared_examples 'all pages have the correct layout' do
+  it 'all pages have the correct layout' do
+    expect(page_analysis.pages.pluck(:size)).to all(eq(page_layout))
+  end
+end
