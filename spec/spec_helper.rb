ede1e1e6dea0fcc30dd3a3ca5c7b476856d2643e
Albrecht Oster <albrecht@oster.online>
2020-12-27 21:20:40 +0100

    fix incorrect coverage number
    exclude vendor files from coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start('rails') do
+  add_filter '/vendor/'
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
