7b4620487eb28b933103e70be23907d075b0791d
Albrecht Oster <albrecht@oster.online>
2021-03-28 20:36:37 +0200

    reflect redeemed coupon credit in order emails

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -16,4 +16,9 @@ module MailerHelper
 
     image_tag attachments[name].url, options
   end
+
+  def number_to_currency(number, options = {})
+    # fix -amount to return '-0.00' when amount is zero
+    super(number.zero? ? 0 : number, options)
+  end
 end
