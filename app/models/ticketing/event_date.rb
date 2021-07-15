fdbd60d280efc600319de450cd66a1c4d3336922
Albrecht Oster <albrecht@oster.online>
2021-07-15 19:13:11 +0200

    initialize covid19_check_in_url after record is initialized

--- app/models/ticketing/event_date.rb
+++ app/models/ticketing/event_date.rb
@@ -9,6 +9,8 @@ module Ticketing
     has_many :tickets, dependent: :nullify, foreign_key: :date_id, inverse_of: :date
     has_many :reservations, dependent: :destroy, foreign_key: :date_id, inverse_of: :date
 
+    before_validation :set_covid19_check_in_url
+
     def self.upcoming
       where('date > ?', Time.current)
     end
@@ -30,23 +32,16 @@ module Ticketing
       statistics[:total]
     end
 
-    def covid19_check_in_url
-      return unless event.covid19_presence_tracing?
-
-      super || begin
-        update(covid19_check_in_url: cwa_check_in_url)
-        cwa_check_in_url
-      end
-    end
-
     private
 
     def statistics
       ticket_stats_for_event(event).dig(:total, id) || { total: 0, percentage: 0 }
     end
 
-    def cwa_check_in_url
-      @cwa_check_in_url ||= CoronaPresenceTracing::CWACheckIn.new(
+    def set_covid19_check_in_url
+      return unless event.covid19?
+
+      self.covid19_check_in_url ||= CoronaPresenceTracing::CWACheckIn.new(
         description: event.name,
         address: event.location.squish,
         start_time: admission_time,
--- spec/models/ticketing/event_date_spec.rb
+++ spec/models/ticketing/event_date_spec.rb
@@ -13,24 +13,26 @@ RSpec.describe Ticketing::EventDate do
   describe '#covid19_check_in_url' do
     subject { date.covid19_check_in_url }
 
-    let(:event) do
-      build(:event, covid19: true, covid19_presence_tracing: presence_tracing,
-                    admission_duration: 33)
-    end
+    let(:event) { create(:event, covid19: covid19, admission_duration: 33) }
     let(:date) do
-      build(:event_date, event: event,
-                         date: Time.zone.parse('2021-05-12 20:00'),
-                         covid19_check_in_url: check_in_url)
+      create(:event_date, event: event, date: Time.zone.parse('2021-05-12 20:00'),
+                          covid19_check_in_url: check_in_url)
     end
     let(:check_in_url) { nil }
 
-    context 'with presence_tracing enabled' do
-      let(:presence_tracing) { true }
+    shared_examples 'does not create a new URL' do
+      it 'does not create a new check-in URL' do
+        expect(CoronaPresenceTracing::CWACheckIn).not_to receive(:new)
+        subject
+      end
+    end
+
+    context 'with a COVID-19 event' do
+      let(:covid19) { true }
 
       context 'when check-in URL has not been generated and saved yet' do
         before do
-          check_in = instance_double('CoronaPresenceTracing::CWACheckIn',
-                                     url: 'https://barfoo')
+          check_in = instance_double('CoronaPresenceTracing::CWACheckIn', url: 'https://barfoo')
           allow(CoronaPresenceTracing::CWACheckIn).to receive(:new).and_return(check_in)
         end
 
@@ -51,17 +53,14 @@ RSpec.describe Ticketing::EventDate do
         end
 
         it 'saves the check-in URL' do
-          expect { subject }.to change { date[:covid19_check_in_url] }.to('https://barfoo')
+          expect(date[:covid19_check_in_url]).to eq('https://barfoo')
         end
       end
 
       context 'when check-in URL has already been generated and saved' do
         let(:check_in_url) { 'https://foo' }
 
-        it 'does not create a new check-in URL' do
-          expect(CoronaPresenceTracing::CWACheckIn).not_to receive(:new)
-          subject
-        end
+        include_examples 'does not create a new URL'
 
         it 'returns the saved check-in URL' do
           expect(subject).to eq('https://foo')
@@ -69,10 +68,12 @@ RSpec.describe Ticketing::EventDate do
       end
     end
 
-    context 'with presence_tracing disabled' do
-      let(:presence_tracing) { false }
+    context 'with a non-COVID-19 event' do
+      let(:covid19) { false }
 
       it { is_expected.to be_nil }
+
+      include_examples 'does not create a new URL'
     end
   end
 end
