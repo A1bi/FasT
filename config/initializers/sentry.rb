2e376cfbf580cf9207803bb37dc2169ac79d735a
Albrecht Oster <albrecht@oster.online>
2022-06-17 14:47:37 +0200

    Improve Sentry SDK configuration
    Disable features unsupported by GlitchTip

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,6 @@ Sentry.init do |config|
   app = Rails.application
   config.dsn = app.credentials.sentry[:dsn] if Settings.sentry.enabled
   config.send_default_pii = true
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.auto_session_tracking = false # not supported by GlitchTip
 end
