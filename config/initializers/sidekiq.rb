48d311459bff66bd88140f8d9e7d27cbea866110
Albrecht Oster <albrecht@oster.online>
2022-11-15 16:20:22 -0600

    Change Postgres and Redis connections
    Postgres now uses the proper hostname instead of a ULA.
    Redis connects via a unix socket.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost/' }
+  config.redis = { url: 'redis:///var/run/redis/redis.sock' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost/' }
+  config.redis = { url: 'redis:///var/run/redis/redis.sock' }
 end
