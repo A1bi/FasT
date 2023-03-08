9fa27e73ff3e74aa2698345686741b601f32900c
Albrecht Oster <albrecht@oster.online>
2023-03-08 23:39:24 +0100

    Refactor statistics view

--- app/helpers/ticketing/statistics_helper.rb
+++ app/helpers/ticketing/statistics_helper.rb
@@ -2,6 +2,18 @@
 
 module Ticketing
   module StatisticsHelper
+    def format_percentage(scope)
+      number_to_percentage(scope&.dig(:percentage) || 0, precision: 0)
+    end
+
+    def format_revenue(scope)
+      number_to_currency(scope&.dig(:revenue) || 0)
+    end
+
+    def format_number(total)
+      number_with_delimiter(total || 0)
+    end
+
     def options_for_events(events, current_event)
       disabled = []
       events = events.map do |event|
