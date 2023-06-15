7ec87674637af7e66d721a9fd7a2659279b36131
Albrecht Oster <albrecht@oster.online>
2023-06-16 00:34:25 +0200

    Migrate statistics

--- app/assets/javascript/controllers/ticketing/statistics_table_controller.js
+++ app/assets/javascript/controllers/ticketing/statistics_table_controller.js
@@ -1,14 +1,11 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  switchTable (event) {
-    const options = this.element.querySelectorAll('.chooser span')
-    options.forEach(option => option.classList.remove('selected'))
-    event.currentTarget.classList.add('selected')
+  switchTable (e) {
+    const tableToShow = e.currentTarget.value
 
-    const tableToShow = event.currentTarget.dataset.table
-    this.element.querySelectorAll('.table').forEach(table => {
-      table.classList.toggle('active', table.classList.contains(tableToShow))
+    document.querySelectorAll('.stats-table').forEach(el => {
+      el.classList.toggle('d-none', !el.classList.contains(tableToShow))
     })
   }
 }
