78503fe0046929e32e045fd3b9d0bfaf04f58864
Albrecht Oster <albrecht@oster.online>
2023-06-16 14:24:51 +0200

    Readd mail header and remove inline image attachments

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -3,18 +3,6 @@
 module MailerHelper
   include RenderHelper
 
-  def attached_image_tag(filename, options = {})
-    name = "inline_#{filename}"
-
-    attachments.inline[name] = File.read(
-      "#{Rails.root}/public/images/mail/#{filename}"
-    )
-
-    options[:style] = "height:#{options[:height]}px;width:auto" if options[:height]
-
-    image_tag attachments[name].url, options
-  end
-
   def number_to_currency(number, options = {})
     # fix -amount to return '-0.00' when amount is zero
     super(number.zero? ? 0 : number, options)
