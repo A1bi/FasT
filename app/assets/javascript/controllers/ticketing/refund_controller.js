ab96af66f5755638b895512f73285fc895ca35c6
Albrecht Oster <albrecht@oster.online>
2023-06-18 13:59:39 +0200

    Fix and improve customer self service

--- app/assets/javascript/controllers/ticketing/refund_controller.js
+++ app/assets/javascript/controllers/ticketing/refund_controller.js
@@ -1,13 +1,11 @@
 import { Controller } from 'stimulus'
 
-import '../../styles/ticketing/refund_controller.sass'
-
 export default class extends Controller {
   static targets = ['bankDetails']
 
   toggleBankDetails (event) {
     const visible = event.currentTarget.value === 'false'
-    this.bankDetailsTarget.classList.toggle('visible', visible)
+    this.bankDetailsTarget.classList.toggle('d-none', !visible)
 
     const inputs = this.bankDetailsTarget.querySelectorAll('input')
     inputs.forEach(input => (input.required = visible))
