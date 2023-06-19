1bed1944ede44d8ed704455dfca0ca5051c54a8f
Albrecht Oster <albrecht@oster.online>
2023-06-19 13:59:37 +0200

    Create release and upload sourcemaps to GlitchTip
    Also remove all JS jobs from CI to avoid losing my mind.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,22 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+// workaround for sentry-cli not supporting FreeBSD, will run it locally instead
+var sentryWebpackPlugin
+try {
+  sentryWebpackPlugin = require('@sentry/webpack-plugin').sentryWebpackPlugin
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') throw e
+}
 
 const environment = require('./environment')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+
+if (sentryWebpackPlugin) {
+  environment.plugins.append('SentryWebpack', sentryWebpackPlugin({
+    url: 'https://glitchtip.a0s.de/',
+    org: 'a0s',
+    project: 'fast',
+    authToken: process.env.SENTRY_AUTH_TOKEN
+  }))
+}
 
 module.exports = environment.toWebpackConfig()
