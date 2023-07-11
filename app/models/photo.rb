edc31374f74cc152c309a01d842e52ba139285f1
Albrecht Oster <albrecht@oster.online>
2023-07-11 19:42:50 +0200

    Implement responsive images for gallery photos
    Provide different sizes and formats for gallery photos based on
    the client's device.
    Add three sizes (300, 600, 900) in each a webp and jpeg version.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Photo < ApplicationRecord
-  has_attached_file :image, styles: { thumb: ['145x145#', :jpg], big: ['600', :jpg] },
-                            only_process: %i[thumb big original]
+  has_attached_file :image, styles: {
+    thumb: ['145x145#', :jpg],
+    **%i[webp jpeg].each_with_object({}) do |format, styles|
+      %i[small medium large].each.with_index(1) do |size, i|
+        styles["#{size}_#{format}".to_sym] = [300 * i, format]
+      end
+    end
+  }
 
   validates_attachment :image, presence: true, content_type:
                          { content_type: %r{\Aimage/(jpe?g|png|hei[cf]|webp)\z} }
