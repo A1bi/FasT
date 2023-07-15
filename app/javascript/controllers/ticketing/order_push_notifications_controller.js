e0186c684084f81fdbcb7a83be8a48353287d0cb
Albrecht Oster <albrecht@oster.online>
2023-07-15 13:08:53 +0200

    Improve user feedback for push notification subscriptions

--- app/javascript/controllers/ticketing/order_push_notifications_controller.js
+++ app/javascript/controllers/ticketing/order_push_notifications_controller.js
@@ -2,39 +2,49 @@ import { Controller } from '@hotwired/stimulus'
 import { toggleDisplay, fetch } from 'components/utils'
 
 export default class extends Controller {
+  static targets = ['spinner', 'button']
   static values = {
     publicKey: String,
-    apiPath: String
+    apiPath: String,
+    successNotificationTitle: String,
+    successNotificationBody: String
   }
 
   /* eslint-disable compat/compat */
   async connect () {
-    this.updateButton()
     if (!this.supported) return
 
     this.serviceWorkerRegistration = await navigator.serviceWorker.register('/ticketing_service_worker.js')
+    this.updateSelf()
   }
 
   async requestNotificationsPermission () {
     const result = await window.Notification.requestPermission()
+    toggleDisplay(this.buttonTarget, false)
 
     if (result === 'granted') {
       await this.subscribe()
     }
 
-    this.updateButton()
+    this.updateSelf()
   }
 
   async subscribe () {
+    toggleDisplay(this.spinnerTarget, true)
+
     const subscription = await this.serviceWorkerRegistration.pushManager.subscribe({
       applicationServerKey: this.publicKeyValue,
       userVisibleOnly: true
     })
 
     await fetch(this.apiPathValue, 'post', JSON.stringify(subscription))
+
+    // eslint-disable-next-line no-new
+    new Notification(this.successNotificationTitleValue, { body: this.successNotificationBodyValue })
+    toggleDisplay(this.spinnerTarget, false)
   }
 
-  updateButton () {
+  updateSelf () {
     toggleDisplay(this.element, this.supported &&
       window.Notification.permission !== 'granted' &&
       window.Notification.permission !== 'denied')
