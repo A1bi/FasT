e63abc4e4b999e3e94c80ee5364f4d92affbc641
Albrecht Oster <albrecht@oster.online>
2023-09-25 15:12:59 +0200

    Update featured events in carousel

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,8 +8,8 @@ class StaticController < ApplicationController
   helper :photos, :events
 
   def index
-    @featured_events = Ticketing::Event.where(identifier: %i[arschlings canterville])
-    @featured_galleries = Rails.env.development? ? [Gallery.last] * @featured_events.count : Gallery.where(id: [76, 77])
+    @featured_events = Ticketing::Event.where(identifier: %i[canterville])
+    @featured_galleries = Rails.env.development? ? [Gallery.last] * @featured_events.count : Gallery.where(id: [77])
     @upcoming_dates = Ticketing::EventDate.upcoming.order(date: :asc)
     @archived_events = Ticketing::Event.including_ticketing_disabled.archived.ordered_by_dates(:desc)
                                        .includes(:location)
