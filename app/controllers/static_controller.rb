9eb182a4649b9117ff5adf58fe992dc46782f1c5
Albrecht Oster <albrecht@oster.online>
2023-09-26 21:41:06 +0200

    Make use of associated galleries on front and event pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,8 +8,7 @@ class StaticController < ApplicationController
   helper :photos, :events
 
   def index
-    @featured_events = Ticketing::Event.where(identifier: %i[canterville])
-    @featured_galleries = Rails.env.development? ? [Gallery.last] * @featured_events.count : Gallery.where(id: [77])
+    @upcoming_events = Ticketing::Event.with_future_dates.ordered_by_dates
     @upcoming_dates = Ticketing::EventDate.upcoming.order(date: :asc)
     @archived_events = Ticketing::Event.including_ticketing_disabled.archived.ordered_by_dates(:desc)
                                        .includes(:location)
