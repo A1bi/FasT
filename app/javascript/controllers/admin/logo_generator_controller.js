3877d852a3da15f850699ffb08333e382c1a6ae7
Albrecht Oster <albrecht@oster.online>
2023-10-08 09:42:52 +0200

    Download generated logos

--- app/javascript/controllers/admin/logo_generator_controller.js
+++ app/javascript/controllers/admin/logo_generator_controller.js
@@ -23,6 +23,18 @@ export default class extends Controller {
     this.updateLogos()
   }
 
+  downloadSvg (event) {
+    const svg = event.target.closest('.col').querySelector('svg')
+    const data = `<?xml version="1.0" standalone="no"?>\r\n${svg.outerHTML}`
+    const url = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(data)}`
+    const link = document.createElement('a')
+    link.download = 'logo.svg'
+    link.href = url
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
   updateColorInputs () {
     this.colorTargets.forEach((color, i) => {
       color.value = this.colors[i]
