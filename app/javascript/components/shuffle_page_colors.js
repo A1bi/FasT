86edbd6ad296c7f310030a3e6e13e82806e0d32a
Albrecht Oster <albrecht@oster.online>
2024-03-25 14:44:15 +0100

    Use event specific colors on event page

--- app/javascript/components/shuffle_page_colors.js
+++ app/javascript/components/shuffle_page_colors.js
@@ -8,9 +8,7 @@ const shuffleColors = () => {
   const colors = generateColors()
 
   colors.forEach((color, i) => {
-    setCSSVariable(`dynamic-color-${i}`, colorToHslCss(color))
-    setCSSVariable(`dynamic-color-${i}-hover`, colorToHslCss(color, 0.8))
-    setCSSVariable(`dynamic-color-${i}-active`, colorToHslCss(color, 0.6))
+    setCSSVariable(`shuffled-color-${i}`, colorToHslCss(color))
   })
 
   document.querySelectorAll('hr').forEach(el => {
