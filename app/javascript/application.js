b35c9c816b0330f321b66717edb8014b4fcfdb3a
Albrecht Oster <albrecht@oster.online>
2024-04-06 14:48:13 +0200

    Set theme color meta tag

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,7 +7,7 @@ import Rails from '@rails/ujs'
 import 'components/header'
 import 'components/carousel'
 import 'components/page_nav'
-import 'components/ruler_color'
+import 'components/theme_color'
 
 initSentry({
   dsn: 'https://2d0c454fb3414c4dafe0ac4736913ec3@glitchtip.a0s.de/1'
---
+++ app/javascript/components/theme_color.js
@@ -0,0 +1,18 @@
+const meta = document.createElement('meta')
+meta.name = 'theme-color'
+document.querySelector('head').appendChild(meta)
+
+const updateThemeColor = () => {
+  const themeColor = window.getComputedStyle(document.querySelector('header')).backgroundColor
+  meta.content = themeColor
+
+  const rulers = document.querySelectorAll('hr')
+  if (rulers.length < 1) return
+
+  const bgImage = window.getComputedStyle(rulers[0]).backgroundImage.replace(/black|hsl|rgb\(.+?\)/, themeColor)
+  rulers.forEach(el => { el.style.backgroundImage = bgImage })
+}
+
+document.addEventListener('DOMContentLoaded', updateThemeColor)
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateThemeColor)
