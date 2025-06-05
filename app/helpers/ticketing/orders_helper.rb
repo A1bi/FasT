beca02ac211c9955a1aa5b6ba0130d6d9e32e0a0
Albrecht Oster <albrecht@oster.online>
2025-06-06 00:16:55 +0200

    Refactor order details and shorten table

--- app/helpers/ticketing/orders_helper.rb
+++ app/helpers/ticketing/orders_helper.rb
@@ -7,5 +7,13 @@ module Ticketing
 
       type.credit_left_for_member(current_user)
     end
+
+    def redeemed_coupon_list(order)
+      return tag.em(t('application.none')) if order.redeemed_coupons.none?
+
+      safe_join(order.redeemed_coupons.map do |coupon|
+        link_to_if(policy(coupon).show?, coupon.recipient.presence || "##{coupon.id}", coupon)
+      end, ', ')
+    end
   end
 end
