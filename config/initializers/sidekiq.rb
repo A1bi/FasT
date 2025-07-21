fc71e4f1ba2cef1b32729416d207b720d455949e
Albrecht Oster <albrecht@oster.online>
2025-07-21 23:23:08 +0200

    Allow Redis Unix socket path to be configurable

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-redis = { path: '/var/run/redis/redis.sock' }
-
 Sidekiq.configure_server do |config|
-  config.redis = redis
+  config.redis = Settings.redis
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = redis
+  config.redis = Settings.redis
 end
